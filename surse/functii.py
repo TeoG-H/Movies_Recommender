d5120ab7ca260b07030025b3362c991fd9505dfc
TeoG-H <tghiorghiu529@gmail.com>
2025-11-06 14:57:33 +0200

    extrage si alte date

--- surse/functii.py
+++ surse/functii.py
@@ -4,17 +4,17 @@ import csv
 import os
 
 def scrap_movies(pages=10):
-    all_items = []
+    all_items = [] 
     HEADERS = {
         "User-Agent": "Mozilla/5.0",
         "Referer": "https://letterboxd.com/films/by/rating/",
     }
     for page in range(pages):
             BASE_URL = f"https://letterboxd.com/films/ajax/by/rating/page/{page+1   }/?esiAllowFilters=true"
-            print(f"📥 Fetching page {page+1}")
+            print(f" Fetching page {page+1}")
 
-            response = requests.get(url=BASE_URL, headers=HEADERS, timeout=10)
-            response.raise_for_status()
+            response = requests.get(url=BASE_URL, headers=HEADERS, timeout=10) #cerere HTTP
+            response.raise_for_status() #arunca eroare daca pagina nu se incarca
             soup = BeautifulSoup(response.text, "html.parser")
 
             for div in soup.select("li.posteritem > div.react-component"):
@@ -34,7 +34,7 @@ def scrap_movies(pages=10):
                         "link": "https://letterboxd.com" + link if link else None
                     })
                 
-    print(f"\n✅ Filme colectate: {len(all_items)}")
+    print(f"\n Filme colectate: {len(all_items)}")
     return all_items
 
 def store_movies(movies):
@@ -47,4 +47,72 @@ def store_movies(movies):
         for item in movies:
             writer.writerow([item["title"], item["rating"], item["link"]])
     
-    print(f"\n✅ Fișier salvat: {output_file}")
+    print(f"\n Fișier salvat: {output_file}")
+
+
+
+def cast():
+
+    HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/120.0.0.0 Safari/537.36",
+    "Referer": "https://letterboxd.com/",
+    "Accept-Language": "en-US,en;q=0.9"
+    }
+
+    url = "https://letterboxd.com/film/band-of-brothers/crew/"
+    try:
+        response = requests.get(url, headers=HEADERS, timeout=10)
+        response.raise_for_status()
+    except Exception as e:
+        print("❌ Nu pot încărca pagina:", e)
+    return
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    print("merge")
+        
+    # des = movie_page.select("div.truncate > p")
+    #     #print(des[0].text)
+
+
+    # c =movie_page.select("div.cast-list  p a")
+    # cast=[a.get_text(strip=True) for a in c]
+    # print("cast\n")
+    # for i in cast:
+    #      print(i)
+
+        
+    # crew= movie_page.select("#tab-crew div.text-sluglist ")
+    # index=crew[:2]
+    # directors=[a.get_text(strip=True) for a in index[0].select("p a")]
+    # producers=[a.get_text(strip=True) for a in index[1].select("p a")]
+
+
+    # print("director\n")
+    # for i in directors:
+    #      print(i)
+    # print("producatori\n")
+    # for i in producers:
+    #     print(i)
+   
+
+
+    # genres= movie_page.select("#tab-genres div.text-sluglist ")
+    # if(len(genres)>1):
+    #     genuri=[a.get_text(strip=True) for a in genres[0].select("p a")]
+    #     themes=[a.get_text(strip=True) for a in genres[1].select("p a")]
+    # else:
+    #     genuri=[a.get_text(strip=True) for a in genres[0].select("p a")]
+    #     themes=[]
+
+
+    # print("genuri")
+    # for i in genuri:
+    #     print(i)
+
+
+    # print("teme")
+    # for i in themes:
+    #     print(i)
+
--- surse/main.py
+++ surse/main.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-from functii import scrap_movies, store_movies
+from functii import scrap_movies, store_movies, cast
 
-movies = scrap_movies()
+# movies = scrap_movies()
 
-# Afișăm primele 20 ca preview:
-print("\nPrimele 20 filme:")
-for i, item in enumerate(movies[:20], start=1):
-    print(f"{i}. {item['title']} ⭐{item['rating']} → {item['link']}")
+# print("\nPrimele 20 filme:")
+# for i, item in enumerate(movies[:20], start=1):
+#     print(f"{i}. {item['title']} {item['rating']} → {item['link']}")
 
-store_movies(movies)
+# store_movies(movies)
+print("merge")
+cast()
+
+   
